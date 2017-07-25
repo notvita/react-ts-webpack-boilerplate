a768d0d917f4b59018034b570e4d530e67a9a869
sparrow <notvita@protonmail.ch>
2017-07-25 11:42:29 +1000

    Fixed stateful Hello component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,16 +2,17 @@ import * as React from 'react';
 
 export interface IHelloProps { compiler: string; framework: string; }
 
+/*
 export const Hello = (props: IHelloProps) => (
   <h1>Hello from {props.compiler} and {props.framework}!</h1>
 );
+*/
 
-/*
 export class Hello extends React.Component<IHelloProps, {}> {
-  render() {
+  public render() {
     return (
-      <h1>Hello from {props.compiler} and {props.framework}!</h1>
-    )
+      <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+    );
   }
 }
-*/
+
